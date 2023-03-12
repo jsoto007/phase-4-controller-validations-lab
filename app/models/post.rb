41308484f59209e81ea7175f4a762cd08e3457da
Jose Soto <jsoto7087@gmail.com>
2023-03-12 13:23:18 -0400

    commit tree f2821e10b02bca10a2743507f1c79a229e819583

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+  validates :content, length: {minimum: 100}
+  # validates :clickbait?
+  # CLICKBAIT_PATTERNS = [
+  #   /Won't Believe/i,
+  #   /Secret/i,
+  #   /Top \d/i,
+  #   /Guess/i
+  # ]
+
+  # def clickbait?
+  #   if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
+
 end
